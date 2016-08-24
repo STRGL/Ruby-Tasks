1234379bb0b6c9acbb1b7994acbf24089641658f
Daryll Clarke <daryllclarke@hotmail.co.uk>
2016-08-24 17:21:10 +0100

    finished first round of tests

--- tdd/application.rb
+++ tdd/application.rb
@@ -3,7 +3,7 @@ require 'Date'
 class Person
 
 	attr_accessor :first_name, :surname, :dob
-	attr_reader :fullname	
+	attr_reader :fullname, :emails, :phone_numbers
 		
 
 	def initialize(name, sname, dob)
@@ -11,6 +11,24 @@ class Person
 		@surname = sname.capitalize
 		@dob = Date.parse(dob)
 		@fullname = @first_name + " " + @surname
+		@emails, @phone_numbers = [], []
 	end
+
+	def add_emails(email)
+		@emails.push(email)
+	end
+
+	def add_phone_numbers(number)
+		@phone_numbers.push(number)
+	end
+
+	def remove_last_email
+		@emails.pop
+	end
+
+	def remove_last_phone_number
+		@phone_numbers.pop
+	end
+
 
 end
--- tdd/spec/person_spec.rb
+++ tdd/spec/person_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper'
 
 describe "Person class - entry in phonebook" do
+
+  before(:each) do
+  	@person = Person.new("joe", "bloggs", "1 Jan 1966")
+  end
+
   it "should store and return personal information" do
-    person = Person.new("joe", "bloggs", "1 Jan 1966")
 
     # Expected Outcomes
     # expect(person.dob.to_s).to be "1996-01-01"
@@ -10,10 +14,38 @@ describe "Person class - entry in phonebook" do
     # expect(person.surname).to be "Bloggs"
     # expect(person.fullname).to be "Joe Bloggs"
 
-    expect(person.dob.to_s).to eq "1966-01-01"
-    expect(person.first_name).to eq "Joe"
-    expect(person.surname).to eq "Bloggs"
-    expect(person.fullname).to eq "Joe Bloggs"
+    expect(@person.dob.to_s).to eq "1966-01-01"
+    expect(@person.first_name).to eq "Joe"
+    expect(@person.surname).to eq "Bloggs"
+    expect(@person.fullname).to eq "Joe Bloggs"
+  end
 
+  it "should store the emails and phone numbers in an array" do
+    @person.add_emails("joe@bloggs.com")
+   	@person.add_emails("bloggs@joe.com")
+   	@person.add_phone_numbers("0712345678")
+    @person.add_phone_numbers("0798765432")
+	
+	# Expected Outcomes
+
+    expect(person.emails).to eq ["joe@bloggs.com", "bloggs@joe.com"]
+    expect(person.phone_numbers).to eq ["0712345678","0798765432"]
+  end
+
+  it "should remove created emails from stored emails" do
+    @person.add_emails("joe@bloggs.com")
+    @person.add_emails("bloggs@joe.com")
+    @person.add_phone_numbers("0712345678")
+    @person.add_phone_numbers("0798765432")
+    elength = @person.emails.length
+    plength = @person.phone_numbers.length
+    @person.remove_last_email
+    @person.remove_last_phone_number
+
+
+
+    #Expected Outcomes
+    expect(person.emails.length).to eq (elength - 1)
+    expect(person.phone_numbers.length).to eq (plength - 1)
   end
 end
